545a49179060980b956b26e8e4016d448c473b89
Nick Baugh <niftylettuce@gmail.com>
2012-12-05 00:55:56 -0500

    Released v0.0.1

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//     YouTube HTML5 Flash Warning Fix
+//     Copyright (c) 2012 Nick Baugh <niftylettuce@gmail.com>
+//     MIT Licensed
+
+// Automatically replaces the Adobe Flash warning message
+// with an embedded HTML5 player (if you have Flash disabled).
+// Silly bug YouTube hasn't fixed yet, so I did.
+//
+// <https://github.com/niftylettuce/youtube-html5-flash-warning-fix>
+
+// * Author: [@niftylettuce](https://twitter.com/#!/niftylettuce)
+// * Source: <https://github.com/niftylettuce/youtube-html5-flash-warning-fix>
+
+// # Youtube HTML5 Flash Warning Fix
+
+;(function($) {
+
+
+  if (!$('#html5-player-messages').hasClass('hid')) {
+    // we could use yt.VIDEO_ID here with chrome ext passing global var
+    var videoID = $('input[name="video_id"]').val();
+    $('#watch-player').replaceWith($('<iframe width="640" height="390" src="http://www.youtube.com/embed/' + videoID + '?autoplay=1" frameborder="0" allowfullscreen></iframe>'));
+  }
+
+})(Zepto);
